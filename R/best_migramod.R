134d852b76ca923380f7afb97d87402d4c21a708
jackowacko <jacksongarces@gmail.com>
2018-03-27 10:38:38 -0500

    Last update functional

--- R/best_migramod.R
+++ R/best_migramod.R
@@ -6,15 +6,19 @@
 #'
 #'@param dataIn Set data for fit
 #'@param model.rc a Object of class MigraMod
-#'@param iter number of iterations for model
+#'@param maxiter Maximum number of iterations for model optimization
+#'@param profile According parameter size of Roger Castro models
 #'
 #'@return a list with named parameters
 #'@examples
-#'data("es_asmr")
-#' data1 <- es_asmr[,c(1,5)]
+#' require(migraR)
+#'require(dplyr)
+#' data("es_asmr")
+#' data1 <- es_asmr[-c(1,2),c(1,5)]
 #' colnames(data1) <- c("x","y")
+#' attach(data1)
 #' model.rc.13 = MigraModel(
-#'   name = 'castro_11',
+#'   name = 'castro_13',
 #'   expr = rc_expression(profile = "thirteen")
 #' )
 #'
@@ -28,27 +32,43 @@
 #' )
 #'
 #' plot(data1, cex=0.1, xlab = 'Age', ylab = 'Standarized Migration Rate')
-#' fitted.val.7 <- best_migramod(dataIn = data1, model.rc =model.rc.7, iter = 50, profile = "seven")
-#' fitted.val.11 <- best_migramod(dataIn = data1, model.rc =model.rc.11, iter = 50, profile = "eleven")
-#' fitted.val.13 <- best_migramod(dataIn = data1, model.rc =model.rc.13, iter = 50, profile = "thirteen")
-#' lines(data1[,1], model.rc.7$value(fitted.val.7$bestParam,data1), col="red")
-#' lines(data1[,1], model.rc.11$value(fitted.val.11$bestParam,data1), col="blue")
-#' lines(data1[,1], model.rc.13$value(fitted.val.13$bestParam,data1), col="gold")
-#' legend("topright",legend = c("Seven","Eleven","Thirteen"),fill = c("red","blue","gold"))
+#' fitted.val.11 <- best_migramod(dataIn = data1, model.rc =model.rc.11, maxite = 5E2, profile = "eleven")
+#' lines(data1[,1], model.rc.11$value(fitted.val.11$bestParam,data1), col="blue", lty=3)
+#' fitted.val.7 <- best_migramod(dataIn = data1, model.rc =model.rc.7, maxite = 5E2, profile = "seven")
+#' lines(data1[,1], model.rc.7$value(fitted.val.7$bestParam,data1), col="blue")
+#' fitted.val.13 <- best_migramod(dataIn = data1, model.rc =model.rc.13, maxite = 5E2, profile = "thirteen")
+#' lines(data1[,1], model.rc.13$value(fitted.val.13$bestParam,data1), col="green")
+#' legend("topright",legend = c("seven","eleven","thirteen"),fill = c("red","blue","green"))
 
 
-best_migramod <- function(dataIn=dataIn, model.rc, iter= 100,profile="eleven"){
+best_migramod <- function(dataIn=dataIn, model.rc, profile="eleven",maxite=1E1){
+  epsilon <- 0.0001
+
    param_0 <- genRandomPar(profile=profile)
   colnames(dataIn) <- c("x","y")
   x <- dataIn[,1]
   y <- dataIn[,2]
   valSim <- fit_migramod(dataIn = dataIn, parameters_0=param_0, model.rc=model.rc )$values
   values.names <- names(valSim)
-  for(i in 1:iter){
+
+  opti.pos <- switch (profile,
+                  seven = 15
+                  ,eleven = 23
+                  ,thirteen = 27
+    )
+
+  opti <- unlist(valSim[opti.pos])
+  counter <-  1
+  pb <- txtProgressBar(counter, maxite,style = 3)
+  while ((abs(opti - epsilon) > 1E-06)  &&  counter < maxite ){
+ # for(i in 1:iter){
     param_0 <- genRandomPar(profile=profile)
     valSim <- rbind(valSim,fit_migramod(dataIn = dataIn, parameters_0=param_0, model.rc )$values)
-
+    opti <- unlist(valSim[nrow(valSim),opti.pos])
+    counter =counter + 1
+    setTxtProgressBar(pb, counter)
   }
+  close(pb)
   rownames(valSim)<- 1:nrow(valSim)
 
   params.n <- switch (profile,
@@ -65,16 +85,21 @@ best_migramod <- function(dataIn=dataIn, model.rc, iter= 100,profile="eleven"){
          mutate_if( is.factor, .funs = function(x)as.numeric(as.character(x)))
 
    bestPar <- dataSimul[which.min(dataSimul$optimResult),params.n$hat]
+   bestParam_0 <- dataSimul[which.min(dataSimul$optimResult),params.n$subzero]
+
         bestPar.mape <- dataSimul[which.min(dataSimul$MAPE),params.n$hat]
     bestOptimRes <- dataSimul[which.min(dataSimul$optimResult), "optimResult"]
     bestMAPE <- dataSimul[which.min(dataSimul$MAPE),"MAPE"]
         names(bestPar) <- names(param_0)
         names(bestPar.mape) <- names(param_0)
-
         bestPar <- sapply(bestPar,as.list)
         names(bestPar) <- names(param_0)
 
-        return(list(bestParam=bestPar, bestOptimRes=bestOptimRes, bestMAPE=bestMAPE,dataSimul=dataSimul))
+        return(list(bestParam=bestPar
+                    ,bestParam_0= bestParam_0
+                    ,bestOptimRes=bestOptimRes
+                    ,bestMAPE=bestMAPE
+                    ,dataSimul=dataSimul))
 
         }
 
--- R/fit_migramod.R
+++ R/fit_migramod.R
@@ -14,32 +14,44 @@ fit_migramod <- function(dataIn=dataIn, parameters_0, model.rc  ){
   colnames(dataIn) <- c("x","y")
   x <- dataIn$x
   y <- dataIn$y
+  #envir <- new.env()
+  #assign("N", 27, envir = env)
       graProof <- model.rc$gradient(p= parameters_0, data = dataIn)
-      while((any(is.na(graProof)) | any(is.nan(graProof)))){
+      while((any(is.na(graProof)) || any(is.nan(graProof)))){
       parameters_0 <-  genRandomPar()
       graProof <- model.rc$gradient(p= parameters_0, data = dataIn)
     }
 
-    fit1 <-  nlminb(parameters_0 , function(p, data){
+    fit1 <-  try(nlminb(parameters_0 , function(p, data){
       #r = -((100/dim(dataIn)[1]) * sum(abs(data$y - mo$value(p,data))/(mo$value(p,data))))
       # r = -((data$y - mo$value(p,data))^2)
       r = -(data$y - model.rc$value(p,x))
       return(r %*% r)
 
     }, gradient = model.rc$gradient
-    , hessian = model.rc$hessian, data=dataIn
-    )
+    ,hessian = model.rc$hessian, data=dataIn, lower = 0, upper = Inf),silent = T)
 
     #while(!(any(is.na(fit1$evaluations[2])) & any(is.nan(fit1$evaluations[2])))){
     #lines(dataIn$x, mo$value(fit1$par,dataIn), col="blue")
     #valSim <- rbind(valSim,c(parameters_0, fit1$par, fit1$objective, fit1$message))
     #}
+    if(any(grepl("Error?", fit1))){
+      mape <- "-1"
+      values <- c(parameters_0, fit1, fit1, fit1, mape)
+      return(list(values=values, model.rc$value(p=parameters_0,data) ))
 
-    mape <- (100/dim(dataIn)[1]) * (sum(abs(dataIn$y - model.rc$value(fit1$par,dataIn))/(model.rc$value(fit1$par,dataIn))))
-    values <- c(parameters_0, fit1$par, fit1$objective, fit1$message, mape)
+    }else{
+
+      mape <- (100/dim(dataIn)[1]) * (sum(abs( model.rc$value(fit1$par,dataIn) -dataIn$y)/(dataIn$y)))
+      values <- c(parameters_0, fit1$par, fit1$objective, fit1$message, mape)
+      return(list(values=values, model.rc$value(p=fit1$par,data) ))
+
+
+    }
+
 
 
    # names(values)[23:25]<- c("objetive","message","mape")
-  return(list(values=values, model.rc$value(p=fit1$par,data) ))
+
 
 }
